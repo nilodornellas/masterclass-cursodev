6aad9cd2daf2136a868fb7614790b90cc99a6c04
Nilo Dornellas <iannilo01@gmail.com>
2024-04-02 17:42:08 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Myllena amo você. Se você me ama, dá uma risadinha 😬</h1>
+  return <h1>Myllena amo você. Se você me ama, dá uma risadinha 😬</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
+
+function teste2() {
+  console.log("formatação erradaaaa!")
+}
